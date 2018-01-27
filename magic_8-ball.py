f8908539cfda49770c8ee0a869517af196b02bf9
karkitko <35235897+karkitko@users.noreply.github.com>
2018-01-27 22:44:44 +0100

    Create magic_8-ball.py

---
+++ magic_8-ball.py
@@ -0,0 +1,17 @@
+# magic ball
+
+import random
+
+answers = [" It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
+
+print("It is Magic 8-Ball. If you don't know what to do, ask yes or no question. ")
+user = input("Do you want to play? Y / N: ")
+    
+if user == "Y":
+    user = input("Write a question here: ")
+    answer = random.choice(answers)
+    print (answer)
+    
+else:
+    print ("Bye")
+    
