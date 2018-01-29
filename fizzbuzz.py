a90e25f980f157d352d5c6e1988a2044e505e3c2
karkitko <35235897+karkitko@users.noreply.github.com>
2018-01-29 19:08:09 +0100

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,14 +1,17 @@
-for x in range(0, 100):
-
+def fizzbuzz(x):
     if x % 3 == 0 and x % 5 == 0:
         hasło_fb = "FizzBuzz"
-        print (x, hasło_fb)
+        print(x, hasło_fb)
     elif x % 3 == 0:
         hasło_f = "Fizz"
-        print (x, hasło_f)
+        print(x, hasło_f)
     elif x % 5 == 0:
         hasło_b = "Buzz"
-        print (x, hasło_b)
+        print(x, hasło_b)
     else:
-        print (str(x))
+        print(x)
+
+
+for x in range(0, 100):
+    fizzbuzz(x)
 
