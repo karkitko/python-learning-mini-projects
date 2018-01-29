44d47a03b2a1ae37fdfd09d0a005fbc4ee0c69c8
karkitko <35235897+karkitko@users.noreply.github.com>
2018-01-29 17:21:25 +0100

    Create fizzbuzz.py

---
+++ fizzbuzz.py
@@ -0,0 +1,14 @@
+for x in range(0, 100):
+
+    if x % 3 == 0 and x % 5 == 0:
+        hasło_fb = "FizzBuzz"
+        print (x, hasło_fb)
+    elif x % 3 == 0:
+        hasło_f = "Fizz"
+        print (x, hasło_f)
+    elif x % 5 == 0:
+        hasło_b = "Buzz"
+        print (x, hasło_b)
+    else:
+        print (str(x))
+
