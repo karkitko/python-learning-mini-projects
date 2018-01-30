e18a2b60dd4866b50afc1175f70dc58b7914754c
karkitko <35235897+karkitko@users.noreply.github.com>
2018-01-30 18:18:19 +0100

    Update fizzbuzz.py
    dodno funckję "main", przeredagowano kod

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,17 +1,16 @@
 def fizzbuzz(x):
-    if x % 3 == 0 and x % 5 == 0:
-        hasło_fb = "FizzBuzz"
-        print(x, hasło_fb)
-    elif x % 3 == 0:
-        hasło_f = "Fizz"
-        print(x, hasło_f)
-    elif x % 5 == 0:
-        hasło_b = "Buzz"
-        print(x, hasło_b)
-    else:
-        print(x)
+    hasło = ""
+    if x % 3 == 0:
+        hasło = "Fizz"
+    if x % 5 == 0:
+        hasło += "Buzz"
+    print(x, hasło)
 
 
-for x in range(0, 100):
-    fizzbuzz(x)
+def main():
+    for x in range(0, 100):
+        fizzbuzz(x)
 
+
+if __name__ == "__main__":
+    main()
